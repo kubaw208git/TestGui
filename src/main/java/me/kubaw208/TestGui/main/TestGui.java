4965b41e018656909d80fd2a8c38c5133a0f4998
Jakub <kubaw208kubaw208@gmail.com>
2022-07-08 00:36:37 +0200

    test commit message2

--- src/main/java/me/kubaw208/TestGui/main/TestGui.java
+++ src/main/java/me/kubaw208/TestGui/main/TestGui.java
@@ -4,6 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class TestGui extends JavaPlugin {
 
+    //Testowy dodatkowy komentarz
     @Override
     public void onEnable() {
         getCommand("gui").setExecutor(new TestGui());
